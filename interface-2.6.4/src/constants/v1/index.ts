659ac7ff7bf9007367a2a3bb33bc256d63405a35
rahul <rahul@HAREKRISHNA>
2024-12-22 22:42:00 -0700

    Add project files.

---
+++ interface-2.6.4/src/constants/v1/index.ts
@@ -0,0 +1,13 @@
+import { Interface } from '@ethersproject/abi'
+import { ChainId } from '@uniswap/sdk'
+import V1_EXCHANGE_ABI from './v1_exchange.json'
+import V1_FACTORY_ABI from './v1_factory.json'
+
+const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {  
+  [ChainId.BSC]: process.env.BSC_V2_FACTORY_ADDRESS || '0xb1cf00A4723daC8513baEF5bDE0A9cf3e36a6dbA' // v2 factory address (This temporary for fix error we not use v1 factory address)
+}
+
+const V1_FACTORY_INTERFACE = new Interface(V1_FACTORY_ABI)
+const V1_EXCHANGE_INTERFACE = new Interface(V1_EXCHANGE_ABI)
+
+export { V1_FACTORY_ADDRESSES, V1_FACTORY_INTERFACE, V1_FACTORY_ABI, V1_EXCHANGE_INTERFACE, V1_EXCHANGE_ABI }
